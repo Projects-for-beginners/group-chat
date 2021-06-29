4d23caeef38ac54ad24cef448c46438694df9b72
innoplusdevs <innoplusdevs@gmail.com>
2021-06-29 12:05:06 -0400

    Basic elements

---
+++ src/index.js
@@ -0,0 +1,107 @@
+import React from "react";
+import ReactDOM from "react-dom";
+/* import React, { useState } from 'react'; */
+import './index.css';
+import SendRoundedIcon from '@material-ui/icons/SendRounded';
+
+
+class Chat extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            allMessage:[]
+        };
+    }
+
+    handleCallback = (FormData) => {
+        if (FormData) {
+         /*    this.setState(prevState => ({
+                messageList: [...prevState.messageList, FormData]
+            })); */
+            this.setState(prevState => ({
+                allMessage: [...prevState.allMessage, FormData ]
+            }));
+        }
+    }
+
+    componentDidMount() {
+        this.timerID = setInterval(
+            () => this.serverMessage(),
+            9000
+        );
+    }
+    componentWillUnmount() {
+        clearInterval(this.timerID);
+    }
+
+    serverMessage() {
+        this.setState(prevState => ({
+            allMessage: [...prevState.allMessage, 'Prueba' ]
+        }));
+    }
+
+    render() {
+        let message;
+        if (this.state.allMessage) {
+            message = <MessageList dataParentToChild={this.state.allMessage} />;
+        }
+
+        return (
+            <div>
+                <Form parentCallback={this.handleCallback} />
+                {message}
+            </div>
+        )
+    }
+}
+
+class Form extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { value: '' };
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(event) { this.setState({ value: event.target.value }); }
+    onTrigger = (event) => {
+        this.props.parentCallback(this.state.value);
+        this.setState({ value: '' });
+        event.preventDefault();
+    }
+
+    render() {
+        return (
+            <div id="box">
+                <form id="form" onSubmit={this.onTrigger}>
+                    <input id="input" placeholder="Your message" autoComplete="off" type="text" value={this.state.value} onChange={this.handleChange} />
+                    <button><SendRoundedIcon /></button>
+                </form>
+            </div>
+        )
+    }
+}
+
+function MessageList(props) {
+    console.log(props.dataParentToChild);
+    const messages = props.dataParentToChild;
+    const messagesList = messages.map((message) =>
+        //key especificada dentro del array.    
+        <Message key={new Date()} value={message} />
+    );
+
+    return (
+        <ul id="messages">
+            {messagesList}
+        </ul>
+    )
+}
+
+
+function Message(props) {
+    return <li><span>{props.value}</span></li>;
+}
+
+ReactDOM.render(
+    <Chat />,
+    document.getElementById('root')
+);
