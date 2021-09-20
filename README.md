e75d79ffeada2710391ee498890beccce9bdcbfb
Ccf <innoplusdevs@gmail.com>
2021-09-20 11:58:36 -0400

    innoplusdevs back

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# group-chat
+# Guava-Chat
+A simple example chat.
+
+
+It is a simple group chat implemented with Node Js, socket.io, ReactJs, and CSS.
+On the left we can see the users online.
--- index.js
+++ index.js
@@ -1,22 +1,55 @@
+const express = require('express');
 const http = require('http');
-const express = require("express");
 const path = require('path');
-const morgan = require("morgan");
-const { Console } = require('console');
+const { Server } = require("socket.io");
 
-const hostname = "localhost";
-const port = 3000;
+const app = express();
 
-const app = express()
+const server = http.createServer(app);
+const io = new Server(server);
 
+const onlineClients = new Set();
+let users = [];
+let messages = [];
 
-app.use(morgan("dev"));
-const staticPath = path.resolve(__dirname, 'Public');
-app.use(express.static(staticPath));
+app.use(express.static(path.join(__dirname, 'public')));
 
-
-const Server = http.createServer(app);
-
-Server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}`);
+server.listen(3000, () => {
+  console.log('listening on *:3000');
 });
+
+io.on('connection', (socket) => {
+  console.info(`Socket ${socket.id} has connected.`);
+  onlineClients.add(socket.id);
+
+  io.to(socket.id).emit('id', socket.id);
+  console.log('id enviado', socket.id);
+
+  socket.on("disconnect", () => {
+    onlineClients.delete(socket.id);
+    users = deleteDisconnectedUser(socket.id);
+
+    io.emit('user join', users);
+    console.info(`Socket ${socket.id} has disconnected.`);
+  });
+
+  socket.on('user join', (user, time) => {
+    users.push([user, socket.id, time]);
+    io.emit('user join', users);
+    io.emit('chat message', messages);
+  });
+
+  socket.on('chat message', (msg, user, id, time) => {
+    messages.push([msg, user, id, time]);
+    io.emit('chat message', messages);
+    console.log(messages);
+
+    console.log(users, 'usuarios');
+  });
+});
+
+const deleteDisconnectedUser = (id) => {
+  return users.filter((user) => {
+    return user[1] !== id;
+  });
+}
